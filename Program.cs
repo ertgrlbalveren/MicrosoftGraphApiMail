32247e690bcdede926c34cb8b91861caaa0d1d32
Ertugrul Balveren <ertgrlbalveren@gmail.com>
2020-08-08 02:46:37 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicrosoftGraphApiMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
